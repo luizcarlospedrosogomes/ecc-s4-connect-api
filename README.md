368ce90be3edfef45d1db286794712ebc072594f
Luiz Carlos Pedroso Gomes <lpedroso@seidor.es>
2023-04-14 23:55:48 -0300

    add xsuaa

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-#
+# Tipos de autenticacao
+- /direct: sem autenticacao, que deve estar desabilitada para ambientes de clientes
+- /xuaa: autenticada com Bearer token do serviço xsuaa do BTP
+# run localhost
+- npm install
+- gere um default-env com os serviços: connectivity, destination e xsuaa
+- adicione a url do serviço gateway do ECC. Ex: sap/opu/odata/sap/<servico>/<endpoint>?sap-language=PT
+- adicione username and password do BTP no arquivo .env
+- gere o token no endpoint /token, veja exemplo no arquivo de testes
+- adicione o token ao arquivo .env
+
+# Exemplos 
+ verifique o arquivo testes
  
--- src/server.js
+++ src/server.js
@@ -1,18 +1,81 @@
 const express = require('express');
-const destination = require('./destination');
+const xsenv = require('@sap/xsenv');
+const xssec = require('@sap/xssec');
 const axios = require("axios");
-const app = express();
+const httpClient = require('@sap-cloud-sdk/http-client');
+const connectivity = require('@sap-cloud-sdk/connectivity');
+const url = require('url');
 
-app.use(async (req, res, next) =>{    
+const destination = require('./destination');
+const logger = require('./logger');
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+xsenv.loadEnv();
+
+const passport = require('passport');
+const xsuaa = xsenv.getServices({ xsuaa: { tag: 'xsuaa' } }).xsuaa;
+passport.use(new xssec.JWTStrategy(xsuaa));
+
+app.use('/direct',async (req, res, next) =>{    
     try {
         const req_options = await destination.build_req_options(req)        
         const result = await axios(req_options);        
         res.status(200).send(result.data);
         next()
     } catch (error) {
-        console.error(`[ERROR] ${error.message}`);        
+        logger.error(`[ERROR] ${error.message}`);        
         res.status(200).send(error);
     }
+})
+
+app.post('/token', async (req, res) =>{
+    const { body } = req
+    const { username, password } = body;
+    
+    if(username === undefined || password === undefined){
+        throw new Error('crendentials not found in body')
+    }
+    try {
+        const params = new url.URLSearchParams({ grant_type: 'password', username, password});               
+        const basicAuth = 'Basic ' + Buffer.from( xsuaa.clientid + ':' + xsuaa.clientsecret).toString('base64');        
+        const result = await axios.get(`${xsuaa.url}/oauth/token?${params.toString()}`, { headers: { Authorization: basicAuth }})        
+        res.statusCode = 200;
+        res.send(result.data.access_token)
+
+    } catch (error) {       
+        logger.error(error.message)
+        res.statusCode = 400;
+        res.send(error)
+    }
+    
+});
+
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }))
+
+app.use('/xsuaa',async (req, res, next) =>{    
+    const jwt = connectivity.retrieveJwt(req);
+    const destinationName  = req.headers['destination-name']; 
+    const props = { url: req.path, data: req['data']}
+    const options = { fetchCsrfToken: false };
+    
+    try {
+        const result = await httpClient.executeHttpRequest(
+            { destinationName,jwt},
+            props,
+            options
+            
+        );
+        res.status(200).send(result.data);
+        next()    
+    } catch (error) {
+        logger.error(`[ERROR] ${error.message}`);        
+        res.status(200).send(error);
+    }
+    
 })
 
 const port = process.env.PORT || 3000;
